5cce706f22b58d2911fddf1e8a84c1a70f9d9dc5
itaiShalom <itai.shalom2@gmail.com>
2017-03-12 17:27:56 +0200

    Initial commit

---
+++ app/src/main/java/com/shalom/itai/selfchat/MainActivity.java
@@ -0,0 +1,72 @@
+package com.shalom.itai.selfchat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+
+    ArrayList<String> chatArr;
+    ListView listView ;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.SendButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                EditText text = (EditText) findViewById(R.id.editText);
+
+                if (chatArr == null) {
+                    chatArr = new ArrayList<String>();
+                }
+                chatArr.add(text.getText().toString());
+                text.setText("");
+                String[] chatArrAdapter = new String[chatArr.size()];
+                chatArrAdapter = chatArr.toArray(chatArrAdapter);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(),
+                        android.R.layout.simple_list_item_1, android.R.id.text1, chatArrAdapter);
+
+                ListView listView = (ListView) findViewById(R.id.listView);
+                listView.setAdapter(adapter);
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
