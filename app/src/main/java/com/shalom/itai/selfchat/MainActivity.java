c25b4548b74c8f5369978622876419da69bca904
itaiShalom <itai.shalom2@gmail.com>
2017-03-16 21:27:41 +0200

    Fix screen rotation

--- app/src/main/java/com/shalom/itai/selfchat/MainActivity.java
+++ app/src/main/java/com/shalom/itai/selfchat/MainActivity.java
@@ -1,6 +1,7 @@
 package com.shalom.itai.selfchat;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -46,7 +47,19 @@ public class MainActivity extends Activity {
         });
 
     }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+/*
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
 
+        }
+        */
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
